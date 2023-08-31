acc8760e42424bb30350e069637f82613dbe5673
Younes Vatankhah <younesvatan78@gmail.com>
2023-08-31 04:19:25 +0330

    customers page and methods added and models for database

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import record
 
 def home(request):
+    records = record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -16,7 +19,7 @@ def home(request):
             messages.success(request,"There was an error loging in please try again...")
             return redirect('home')
     else:    
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records':records})
 
 
 def logout_user(request):
@@ -25,4 +28,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request,'register.html',{})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Auth user and login to site
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('home')
+    else:
+        form = SignUpForm()   
+        return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = record.objects.get(id=pk )
